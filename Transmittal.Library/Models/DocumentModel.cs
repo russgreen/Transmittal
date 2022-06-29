7cc290ec35acf39f4257bce1aa0a3eb98f1c8ccf
Russ Green <1886088+russgreen@users.noreply.github.com>
2022-06-29 06:06:09 +0100

    custom validation attribute

--- Transmittal.Library/Models/DocumentModel.cs
+++ Transmittal.Library/Models/DocumentModel.cs
@@ -1,8 +1,12 @@
-﻿namespace Transmittal.Library.Models;
+﻿using Transmittal.Library.Validation;
+
+namespace Transmittal.Library.Models;
 public class DocumentModel : TransmittalItemModel
 {
     public string FilePath { get; set; }
 
+    [BeginsWith(nameof(DocumentModel.DrgProj), 
+        ErrorMessage = "The file name doesn't begin with the project number. Are you sure you want to record a transmittal?")]
     public string FileName { get; set; }
 
     public DocumentTypeModel DocumentType { get; set; }
---
+++ Transmittal.Library/Validation/BeginsWithAttribute.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Text;
+
+namespace Transmittal.Library.Validation;
+public sealed class BeginsWithAttribute : ValidationAttribute
+{
+    public string PropertyName { get; }
+
+    private const string _defaultErrorMessage = "The value does not begin with the expected value";
+
+    public BeginsWithAttribute(string propertyName)
+    {
+        PropertyName = propertyName;
+
+    }
+
+    protected override ValidationResult IsValid(object value, ValidationContext validationContext)
+    {
+        object
+            instance = validationContext.ObjectInstance,
+            otherValue = instance.GetType().GetProperty(PropertyName).GetValue(instance);
+
+        if (value == null)
+        {
+            return ValidationResult.Success;
+        }
+
+        if (((IComparable)value).ToString().StartsWith(otherValue.ToString()))
+        {
+            return ValidationResult.Success;
+        }
+
+        return new(ErrorMessage ?? _defaultErrorMessage);
+
+    }
+
+}
