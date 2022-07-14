6761350e00e65beb457b772805bb1a1b35e0f2c2
Russ Green <1886088+russgreen@users.noreply.github.com>
2022-07-14 07:43:16 +0100

    browse for custom parameters to use in a project

---
+++ Transmittal/Models/ParameterDataModel.cs
@@ -0,0 +1,11 @@
+﻿using Autodesk.Revit.DB;
+
+namespace Transmittal.Models;
+internal class ParameterDataModel
+{
+    public ElementId ID { get; set; }
+    public string Name { get; set; }
+    public string Guid { get; set; }
+    public ElementBinding Binding { get; set; }
+    public Definition Definition { get; set; }
+}
