b8a7b7f55cf9a454d3a55741bb7f11e10cdacf8b
Russ Green <1886088+russgreen@users.noreply.github.com>
2023-03-19 11:27:19 +0000

    initual unit tests added to solution

---
+++ Transmittal.Library.Tests/DateExtensionsTests.cs
@@ -0,0 +1,81 @@
+﻿using Transmittal.Library.Extensions;
+
+namespace Transmittal.Library.Tests;
+public class DateExtensionsTests
+{
+    [Theory]
+    [InlineData("23.03.18", 2023, 3, 18)]
+    [InlineData("10.10.10", 2010, 10, 10)]
+    public void ToStringYYMMDDEx_ShouldReturnFormattedString(string expected, int year, int month, int day)
+    {
+        // Arrange
+        var date = new DateTime(year, month, day);
+
+        // Act
+        var result = date.ToStringYYMMDDEx();
+
+        // Assert
+        Assert.Equal(expected, result);
+    }
+
+    [Theory]
+    [InlineData("230318", 2023, 3, 18)]
+    [InlineData("101010", 2010, 10, 10)]
+    public void ToStringYYMMDD_ShouldReturnFormattedString(string expected, int year, int month, int day)
+    {
+        // Arrange
+        var date = new DateTime(year, month, day);
+
+        // Act
+        var result = date.ToStringYYMMDD();
+
+        // Assert
+        Assert.Equal(expected, result);
+    }
+
+    [Theory]
+    [InlineData("23", 2023, 3, 18)]
+    [InlineData("10", 2010, 10, 10)]
+    public void ToStringYY_ShouldReturnFormattedString(string expected, int year, int month, int day)
+    {
+        // Arrange
+        var date = new DateTime(year, month, day);
+
+        // Act
+        var result = date.ToStringYY();
+
+        // Assert
+        Assert.Equal(expected, result);
+    }
+
+    [Theory]
+    [InlineData("03", 2023, 3, 18)]
+    [InlineData("10", 2010, 10, 10)]
+    public void ToStringMM_ShouldReturnFormattedString(string expected, int year, int month, int day)
+    {
+        // Arrange
+        var date = new DateTime(year, month, day);
+
+        // Act
+        var result = date.ToStringMM();
+
+        // Assert
+        Assert.Equal(expected, result);
+    }
+
+    [Theory]
+    [InlineData("18", 2023, 3, 18)]
+    [InlineData("01", 2010, 10, 1)]
+    public void ToStringDD_ShouldReturnFormattedString(string expected, int year, int month, int day)
+    {
+        // Arrange
+        var date = new DateTime(year, month, day);
+
+        // Act
+        var result = date.ToStringDD();
+
+        // Assert
+        Assert.Equal(expected, result);
+    }
+
+}
---
+++ Transmittal.Library.Tests/ModelExtensionsTests.cs
@@ -0,0 +1,32 @@
+﻿using Transmittal.Library.Extensions;
+
+
+namespace Transmittal.Library.Tests;
+public class ModelExtensionsTests
+{
+    [Fact]
+    public void CopyPropertiesTo_ShouldCopyPropertiesFromOneObjectToAnother()
+    {
+        // Arrange
+        var fromObject = new { FirstName = "John", LastName = "Doe", Age = 30 };
+        var toObject = new Person();
+
+        // Act
+        fromObject.CopyPropertiesTo(toObject);
+
+        // Assert
+        Assert.Equal(fromObject.FirstName, toObject.FirstName);
+        Assert.Equal(fromObject.LastName, toObject.LastName);
+        Assert.Equal(fromObject.Age, toObject.Age);
+    }
+
+
+    private class Person
+    {
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public int Age { get; set; }
+    }
+}
+
+
