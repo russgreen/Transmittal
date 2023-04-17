4fa91d7c416b97e94a741fc40bc841770843b8ac
Russ Green <1886088+russgreen@users.noreply.github.com>
2023-04-17 07:42:59 +0100

    added ability to check for updates but not implemented as not required on every run.

--- Transmittal.Desktop/ViewModels/MainViewModel.cs
+++ Transmittal.Desktop/ViewModels/MainViewModel.cs
@@ -6,6 +6,7 @@ using System.Linq;
 using System.Reflection;
 using System.Text;
 using System.Threading.Tasks;
+using Transmittal.Library.Enums;
 using Transmittal.Library.Extensions;
 using Transmittal.Library.Services;
 using Transmittal.Library.ViewModels;
@@ -14,6 +15,7 @@ namespace Transmittal.Desktop.ViewModels;
 internal partial class MainViewModel : BaseViewModel
 {
     private readonly ISettingsService _settingsService = Host.GetService<ISettingsService>();
+    private readonly ISoftwareUpdateService _softwareUpdateService = Host.GetService<ISoftwareUpdateService>();
 
     public string WindowTitle { get; private set; }
 
@@ -23,6 +25,9 @@ internal partial class MainViewModel : BaseViewModel
 
     [ObservableProperty]
     private bool _hasDatabase = true;
+
+    [ObservableProperty]
+    private string _message;
 
     public MainViewModel()
     {
@@ -36,6 +41,18 @@ internal partial class MainViewModel : BaseViewModel
         if (Database == "[NONE]")
         {
             HasDatabase = false;
+        }
+
+        //CheckForUpdates();
+    }
+
+    //don't want this to run every time the app launches
+    private async Task CheckForUpdates()
+    {    
+        await _softwareUpdateService.CheckUpdates();
+        if (_softwareUpdateService.State == SoftwareUpdateState.ReadyToInstall)
+        {
+            Message = $"New version {_softwareUpdateService.NewVersion} is ready to install.";
         }
     }
 
