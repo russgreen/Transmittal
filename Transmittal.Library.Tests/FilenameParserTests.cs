cfd04298f2d07a0a7d35351d998518733b771792
Russ Green <1886088+russgreen@users.noreply.github.com>
2023-05-20 12:01:59 +0100

    updated parser to pass all unit tests

--- Transmittal.Library.Tests/FilenameParserTests.cs
+++ Transmittal.Library.Tests/FilenameParserTests.cs
@@ -16,9 +16,10 @@ public class FilenameParserTests
     [InlineData("0001-ORI-XX-D01-GA-A-0003 P01 Site Analysis Plan.pdf", "<ProjNo>-<Originator>-<Volume>-<Level>-<Type>-<Role>-<SheetNo> <Rev> <SheetName2>", "0001", "ORI", "A", "XX", "D01", "GA", "0003", "S2", "", "P01", "Site Analysis Plan")]
     [InlineData("PROJ-ORI-XX-00-GA-A-0003", "<ProjNo>-<Originator>-<Volume>-<Level>-<Type>-<Role>-<SheetNo>", "PROJ", "ORI", "A", "XX", "00", "GA", "0003", "S2", "", "", "")]
     [InlineData("PROJ-ORI-XX-00-GA-A-0003-C02", "<ProjNo>-<Originator>-<Volume>-<Level>-<Type>-<Role>-<SheetNo>-<Rev>", "PROJ", "ORI", "A", "XX", "00", "GA", "0003", "S2", "", "C02", "")]
-    //[InlineData("0001-ORI-XX-D01-GA-A-0003.pdf", "<ProjNo>-<Originator>-<Volume>-<Level>-<Type>-<Role>-<SheetNo>-<SheetName>-<Status>-<Rev>", "0001", "ORI", "A", "XX", "D01", "GA", "0003", "S2", "", "P01", "Site Analysis Plan")]
+    //[InlineData("0001-ORI-XX-D01-GA-A-0003.pdf", "<ProjNo>-<Originator>-<Volume>-<Level>-<Type>-<Role>-<SheetNo>-<SheetName>-<Status>-<Rev>", "0001", "ORI", "A", "XX", "D01", "GA", "0003", "", "", "", "")]
     [InlineData("PROJ-ORI-XX-00-GA-A-0003-SheetName-ProjectName-S1-C02", "<ProjNo>-<Originator>-<Volume>-<Level>-<Type>-<Role>-<SheetNo>-<SheetName>-<ProjName>-<Status>-<Rev>", "PROJ", "ORI", "A", "XX", "00", "GA", "0003", "S1", "", "C02", "Sheet Name")]
     [InlineData("0001-ORI-XX-D01-GA-A-0003-SiteAnalysisPlan-S2-P01.pdf", "<ProjNo>-ORI-<Volume>-<Level>-<Type>-<Role>-<SheetNo>-<SheetName>-<Status>-<Rev>", "0001", "ORI", "A", "XX", "D01", "GA", "0003", "S2", "", "P01", "Site Analysis Plan")]
+    [InlineData("P00 000-OR-ZZ-ZZ-DR-A-0601-P02_Planning Detail 01 - Edge of Slab, Brick Facade.pdf", "<ProjNo>-<Originator>-<Volume>-<Level>-<Type>-<Role>-<SheetNo>-<Rev>_<SheetName2>", "P00 000", "OR", "A", "ZZ", "ZZ", "DR", "0601", "", "", "P02", "Planning Detail 01 - Edge of Slab, Brick Facade")]
     public void DocumentModel_ShouldReturnExpectedResult(string filePath, 
         string exportRule, 
         string projectNumber,
