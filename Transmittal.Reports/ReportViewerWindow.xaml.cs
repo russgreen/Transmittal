39ff4853058b19062cbb4bcacf171034a4e7046e
Russ Green <1886088+russgreen@users.noreply.github.com>
2023-07-28 07:16:20 +0100

    changed from windows forms to a windows forms host in a WPF window. closes #50

--- Transmittal.Reports/ReportViewerWindow.xaml.cs
+++ Transmittal.Reports/ReportViewerWindow.xaml.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Microsoft.Reporting.WinForms;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -25,18 +26,51 @@ public partial class ReportViewerWindow : Window
         InitializeComponent();
     }
 
+
     public ReportViewerWindow(string folderPath, string fileName)
     {
         InitializeComponent();
 
         _filePathName = System.IO.Path.Combine(folderPath, fileName);
 
-        this.reportViewer.ExportSettings = new BoldReports.Writer.ExportSettings();
-        this.reportViewer.ExportSettings.FileName = _filePathName;
+        reportViewer.ReportExport += ReportViewer_ReportExport;
     }
 
-    private void reportViewer_ReportExport(object sender, BoldReports.Windows.ReportExportEventArgs e)
+    private void ReportViewer_ReportExport(object sender, ReportExportEventArgs e)
     {
-        var format = e.ExportFormatType;
+        e.Cancel = true;
+        string mimeType;
+        string encoding;
+        string fileNameExtension;
+        string[] streams;
+        Microsoft.Reporting.WinForms.Warning[] warnings;
+
+        Microsoft.Reporting.WinForms.Report report;
+        if (reportViewer.ProcessingMode == Microsoft.Reporting.WinForms.ProcessingMode.Local)
+        {
+            report = reportViewer.LocalReport;
+        }
+        else
+        {
+            report = reportViewer.ServerReport;
+        }
+
+        var bytes = report.Render(e.Extension.Name, e.DeviceInfo,
+                        Microsoft.Reporting.WinForms.PageCountMode.Actual, out mimeType,
+                        out encoding, out fileNameExtension, out streams, out warnings);
+
+        var path = $@"{_filePathName}.{fileNameExtension}";
+        System.IO.FileInfo file = new System.IO.FileInfo(path);
+        file.Directory.Create();
+
+        try
+        {
+            System.IO.File.WriteAllBytes(path, bytes);
+            //MessageBox.Show($"The report was exported to file {path}");
+        }
+        catch (Exception)
+        {
+            //MessageBox.Show($"The report could not be saved to {path}. Check the file is not open and you have write access to the folder.");
+        }
     }
 }
