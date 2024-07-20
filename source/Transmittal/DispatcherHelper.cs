e79df48070198f67b2af3abdfa9248ec525ae890
Russ Green <1886088+russgreen@users.noreply.github.com>
2024-07-20 08:45:28 +0100

    version bump

--- source/Transmittal/DispatcherHelper.cs
+++ source/Transmittal/DispatcherHelper.cs
@@ -1,10 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Security.Permissions;
-using System.Text;
-using System.Threading.Tasks;
-using System.Windows.Threading;
+﻿using System.Windows.Threading;
 
 namespace Transmittal;
 internal static class DispatcherHelper
