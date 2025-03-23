1184eb37ce164c1cc46b89045cd16d76dfa67a73
Russ Green <1886088+russgreen@users.noreply.github.com>
2025-03-23 17:04:36 +0000

    Refactor code for clarity and consistency
    Updated namespace declarations and reformatted class definitions across multiple files. Improved readability of methods, logging configuration, and service registration. Added `[ValueConversion]` attribute for clarity in `InverseBooleanToVisibilityConverter`. Enhanced maintainability by restructuring methods in `NewCompanyViewModel` and `ParameterSelectorViewModel`.

--- source/Transmittal/Converters/InverseBooleanToVisibilityConverter.cs
+++ source/Transmittal/Converters/InverseBooleanToVisibilityConverter.cs
@@ -3,15 +3,14 @@ using System.Globalization;
 using System.Windows;
 using System.Windows.Data;
 
-namespace Transmittal.Converters
-{
-    [ValueConversion(typeof(bool), typeof(Visibility))]
-    internal class InverseBooleanToVisibilityConverter : IValueConverter
-    {
-        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
-            => (bool?)value == false ? Visibility.Visible : Visibility.Collapsed;
+namespace Transmittal.Converters;
 
-        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
-            => throw new NotImplementedException();
-    }
+[ValueConversion(typeof(bool), typeof(Visibility))]
+internal class InverseBooleanToVisibilityConverter : IValueConverter
+{
+    public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        => (bool?)value == false ? Visibility.Visible : Visibility.Collapsed;
+
+    public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        => throw new NotImplementedException();
 }
