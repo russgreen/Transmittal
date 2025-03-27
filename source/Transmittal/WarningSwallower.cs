327e23c33307c327ced70a3fc77e4e149272fd28
Russ Green <1886088+russgreen@users.noreply.github.com>
2025-03-27 19:04:42 +0000

    Refactor WarningSwallower.cs for clarity and efficiency
    - Added `using System;` and `using System.Linq;` directives.
    - Moved namespace declaration for `Transmittal`.
    - Removed commented-out `FailureSeverityList` property.
    - Simplified failure message processing logic in `PreprocessFailures`.

--- source/Transmittal/WarningSwallower.cs
+++ source/Transmittal/WarningSwallower.cs
@@ -1,28 +1,18 @@
-﻿namespace Transmittal;
+﻿using System;
+using System.Linq;
+
+namespace Transmittal;
 
 public class WarningSwallower : Autodesk.Revit.DB.IFailuresPreprocessor
 {
-    //private List<Autodesk.Revit.DB.FailureSeverity> FailureSeverityList
-    //{
-    //    get
-    //    {
-    //        var list = new List<Autodesk.Revit.DB.FailureSeverity>
-    //        { 
-    //            Autodesk.Revit.DB.FailureSeverity.Warning,
-    //            Autodesk.Revit.DB.FailureSeverity.Error
-    //        };
-    //        return list;
-    //    }
-    //}
-
     public Autodesk.Revit.DB.FailureProcessingResult PreprocessFailures(Autodesk.Revit.DB.FailuresAccessor FailuresAccessor)
     {
         var msgAccessorList = FailuresAccessor.GetFailureMessages();
         foreach (Autodesk.Revit.DB.FailureMessageAccessor msgAccessor in msgAccessorList)
         {
             _ = FailuresAccessor.GetTransactionName();
-            if (msgAccessor.GetDescriptionText().ToString().Contains("revit will use raster printing", StringComparison.CurrentCultureIgnoreCase) == true | 
-                msgAccessor.GetDescriptionText().ToLower().Contains("the <in-session> print settings will be used", StringComparison.CurrentCultureIgnoreCase))
+            if (msgAccessor.GetDescriptionText().ToString().ToLower() == "revit will use raster printing" | 
+                msgAccessor.GetDescriptionText().ToLower().ToLower() == "the <in-session> print settings will be used")
             {
                 FailuresAccessor.DeleteWarning(msgAccessor);
             }
