2e9538c14cabc5ee2bb3124f74946afe520c5b9b
Russ Green <1886088+russgreen@users.noreply.github.com>
2025-04-26 09:22:40 +0100

    Refactor build targets and update .NET framework
    - Changed `Compile` target to be triggered by `Clean`.
    - Updated `Installer` target to be triggered by `Sign` with a branch check.
    - Modified `Sign` target to be triggered by `Compile`.
    - Altered `Main` method to execute `Clean` instead of `Installer`.
    - Upgraded target framework from `net8.0` to `net9.0`.

--- build/Build.Installer.cs
+++ build/Build.Installer.cs
@@ -10,7 +10,7 @@ using System.Linq;
 partial class Build
 {
     Target Installer => _ => _
-        .DependsOn(Sign)
+        .TriggeredBy(Sign)
         .OnlyWhenStatic(() => GitRepository.IsOnMainOrMasterBranch())
         .Executes(() =>
         {
