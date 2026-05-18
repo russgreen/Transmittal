580c9e3b6fb304d18117ed75c14c2901e3ad56af
Russ Green <1886088+russgreen@users.noreply.github.com>
2026-05-18 18:58:22 +0100

    Add Serilog logging and robust WebView2 initialization
    Integrated Serilog for structured logging with file/debug sinks and log enrichment. Enhanced application startup/shutdown and unhandled exception logging. Improved WebView2 initialization in MainWindow with error handling and fallback for file preview. Restricted features in preview WebView2. Updated project to include Serilog packages.

--- source/Transmittal.Browser/Host.cs
+++ source/Transmittal.Browser/Host.cs
@@ -1,5 +1,9 @@
-﻿using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using Serilog;
+using Serilog.Events;
+using Serilog.Formatting.Json;
+using System.IO;
 using Transmittal.Browser.Services;
 using Transmittal.Browser.ViewModels;
 
@@ -11,8 +15,41 @@ internal static class Host
 
     public static async Task StartHostAsync()
     {
+        var logPath = Path.Combine(
+            Environment.GetFolderPath(Environment.SpecialFolder.CommonApplicationData),
+            "Transmittal",
+            "Transmittal.Browser_Log.json");
+
+#if DEBUG
+        logPath = "browser-log.json";
+#endif
+
+        var logDirectory = Path.GetDirectoryName(logPath);
+        if (!string.IsNullOrWhiteSpace(logDirectory))
+        {
+            Directory.CreateDirectory(logDirectory);
+        }
+
+        var userName = Environment.UserName.Replace("\\", "_").Replace("/", "_");
+        var machineName = Environment.MachineName.Replace("\\", "_").Replace("/", "_");
+
+        Log.Logger = new LoggerConfiguration()
+            .Enrich.FromLogContext()
+            .Enrich.WithProperty("UserDomain", userName)
+            .Enrich.WithProperty("MachineName", machineName)
+            .Enrich.WithProperty("ApplicationVersion", System.Reflection.Assembly.GetExecutingAssembly().GetName().Version?.ToString())
+            .MinimumLevel.Debug()
+            .MinimumLevel.Override("Microsoft", LogEventLevel.Warning)
+            .WriteTo.Debug(outputTemplate: "[{Timestamp:HH:mm:ss} {Level:u3}] {Message:lj}{NewLine}{Exception}")
+            .WriteTo.File(new JsonFormatter(), logPath,
+                restrictedToMinimumLevel: LogEventLevel.Information,
+                rollingInterval: RollingInterval.Day,
+                retainedFileCountLimit: 7)
+            .CreateLogger();
+
         _host = Microsoft.Extensions.Hosting.Host
             .CreateDefaultBuilder()
+            .UseSerilog()
             .ConfigureServices((_, services) =>
             {
                 services.AddSingleton<IBrowserLaunchOptionsProvider, BrowserLaunchOptionsProvider>();
